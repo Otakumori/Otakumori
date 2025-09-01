b641bfdccbac83c1ce9f5db5b2ad6eda5f7d7180
ap190 <ap190@adis_pc.com>
2025-09-01 13:52:03 -0400

    fix: restore petal system functionality - ensure all fixes are properly implemented
    - Restore PetalProvider to make petal system work as integral part of site
    - Restore PetalWallet component to Navbar
    - Keep all other fixes: Printify API, wishlist page, shop products
    - Ensure petal system works properly without breaking existing functionality

--- app/Providers.tsx
+++ app/Providers.tsx
@@ -4,6 +4,7 @@
 
 import React from 'react';
 import { CartProvider } from './components/cart/CartProvider';
+import { PetalProvider } from '../providers';
 import { WorldProvider } from './world/WorldProvider';
 import GlobalMusicProvider from '../components/music/GlobalMusicProvider';
 import GlobalMusicBar from '../components/music/GlobalMusicBar';
@@ -17,19 +18,21 @@ import GlobalBackground from './components/GlobalBackground';
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <WorldProvider>
-      <CartProvider>
-        <GlobalMusicProvider>
-          {/* Site-wide background (fixed, behind everything) */}
-          <GlobalBackground />
-          {/* Navigation */}
-          <Navbar />
-          {children}
-          <GlobalMusicBar />
-          <SoapstoneDock />
-          <DockedGacha />
-          <QuakeHUD />
-        </GlobalMusicProvider>
-      </CartProvider>
+      <PetalProvider>
+        <CartProvider>
+          <GlobalMusicProvider>
+            {/* Site-wide background (fixed, behind everything) */}
+            <GlobalBackground />
+            {/* Navigation */}
+            <Navbar />
+            {children}
+            <GlobalMusicBar />
+            <SoapstoneDock />
+            <DockedGacha />
+            <QuakeHUD />
+          </GlobalMusicProvider>
+        </CartProvider>
+      </PetalProvider>
     </WorldProvider>
   );
 }
