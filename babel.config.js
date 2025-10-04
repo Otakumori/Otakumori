dc63da247acf638cee318fa246663532dbeed3ac
ap190 <ap190@adis_pc.com>
2025-10-04 13:20:46 -0400

    fix: remove babel config to resolve SWC conflicts
    - Remove babel.config.js to allow Next.js SWC compiler
    - Remove babel-plugin-transform-remove-console dependency
    - This resolves next/font and @react-three/drei compatibility issues

--- babel.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  presets: ['next/babel'],
-  plugins: [
-    ...(process.env.NODE_ENV === 'production'
-      ? [['babel-plugin-transform-remove-console', { exclude: ['error', 'warn'] }]]
-      : []),
-  ],
-};
