93dd96b87ab4ce97eebf409134ff0679a6758816
Adi <ap1903@hotmail.com>
2026-05-04 16:20:53 -0400

    Isolate commerce core root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,10 @@
 import './globals.css';
 import type { ReactNode } from 'react';
-import { headers } from 'next/headers';
-import ClerkProviderWrapper from './providers/ClerkProviderWrapper';
-import AppShell from './AppShell';
 
-interface RootLayoutProps {
-  children: ReactNode;
-}
-
-export default async function RootLayout({ children }: RootLayoutProps) {
-  const headersList = await headers();
-  const nonce = headersList.get('x-nonce') ?? undefined;
-
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <ClerkProviderWrapper nonce={nonce || undefined}>
-      <html lang="en">
-        <body>
-          <AppShell>{children}</AppShell>
-        </body>
-      </html>
-    </ClerkProviderWrapper>
+    <html lang="en">
+      <body>{children}</body>
+    </html>
   );
 }
