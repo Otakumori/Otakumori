f147f457c4893a30abcbacfd4cc3379bb4282690
Adi <ap1903@hotmail.com>
2026-05-04 16:28:52 -0400

    Move commerce core customer route into route group

---
+++ app/(commerce-core)/commerce-core/account/page.tsx
@@ -0,0 +1,5 @@
+import { CommerceCoreShell } from '../../../commerce-core/_components/CommerceCoreShell';
+
+export default function CommerceCoreCustomerPage() {
+  return <CommerceCoreShell mode="account" />;
+}
