94588e253fd03dcd54aa0de114dc02fb9e7e2e5d
Adi <ap1903@hotmail.com>
2026-05-05 00:18:39 -0400

    Add reusable legacy site route layout

---
+++ app/LegacySiteRouteLayout.tsx
@@ -0,0 +1,15 @@
+import type { ReactNode } from 'react';
+import { headers } from 'next/headers';
+import ClerkProviderWrapper from './providers/ClerkProviderWrapper';
+import FullAppShell from './FullAppShell';
+
+export default async function LegacySiteRouteLayout({ children }: { children: ReactNode }) {
+  const headersList = await headers();
+  const nonce = headersList.get('x-nonce') ?? undefined;
+
+  return (
+    <ClerkProviderWrapper nonce={nonce}>
+      <FullAppShell>{children}</FullAppShell>
+    </ClerkProviderWrapper>
+  );
+}
