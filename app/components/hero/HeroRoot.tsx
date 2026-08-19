4d4698b3c2a6ff14b5051cb4974306886ea681ea
Adrianna Page <ap1903@hotmail.com>
2026-08-19 17:14:30 -0400

    fix: size home shell to rendered world art

--- app/components/hero/HeroRoot.tsx
+++ app/components/hero/HeroRoot.tsx
@@ -1,12 +1,13 @@
 import HeroScene from './HeroScene';
 import HeroOverlay from './HeroOverlay';
 import HeroContent from './HeroContent';
+import layoutStyles from './HeroLayout.module.css';
 
 export default function HeroRoot() {
   return (
     <section
       id="main-content"
-      className="relative isolate w-full overflow-hidden bg-[#080611]"
+      className={`${layoutStyles.heroRoot} relative isolate w-full overflow-hidden bg-[#080611]`}
       aria-labelledby="home-hero-title"
     >
       <HeroScene />
